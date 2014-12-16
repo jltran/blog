f404a7aca41832f528c05540397b9757150e07a4
jltran <guardian@berkeley.edu>
2014-12-15 18:29:49 -0800

    Changed grunt functionality to include Sass and Bourbon

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 module.exports = function(grunt) {
 
@@ -10,30 +10,28 @@ module.exports = function(grunt) {
       src: 'src',
       dist: 'dist'
     },
-      
-    copy: {
-      main: {
-        src: '<%= config.src %>/styles/*.css',
-        dest: '<%= config.dist %>/assets/css/app.css',
-      }
+    
+    sass: {
+        dist: {
+            files: [{
+                expand: true,
+                cwd: '<%= config.src %>/styles',
+                src: ['*.sass'],
+                dest: '<%= config.dist %>/assets/css',
+                ext: '.css'
+            }]
+        }
     },
 
     cssmin: {
       minify: {
         expand: true,
         cwd: '<%= config.dist %>/assets/css/',
-        src: ['*.css', '!*.min.css'],
+        src: ['*.css'],
         dest: '<%= config.dist %>/assets/css/',
         ext: '.min.css'
       }
     },
-      
-    bower: {
-      dev: {
-        css_dest: '<%= config.dist %>/assets/styles'
-      }
-    },
-
 
     autoprefixer: {
       single_file: {
@@ -48,8 +46,6 @@ module.exports = function(grunt) {
     concat: {
       app: {
         src: [
-          'bower_components/jquery/dist/jquery.js',
-          'bower_components/bigtext/dist/bigtext.js',
           '<%= config.src %>/js/app.js'
         ],
         dest: '<%= config.dist %>/assets/js/app.js'
@@ -63,15 +59,37 @@ module.exports = function(grunt) {
       }
     },
 
-
+    assemble: {
+      pages: {
+        options: {
+          flatten: true,
+          assets: '<%= config.dist %>/assets',
+          layout: '<%= config.src %>/templates/layouts/default.hbs',
+          data: '<%= config.src %>/data/*.{json,yml}',
+          partials: '<%= config.src %>/templates/partials/*.hbs'
+        },
+        files: {
+          '<%= config.dist %>/': ['<%= config.src %>/templates/pages/*.hbs']
+        }
+      }
+    },
+      
     watch: {
       assemble: {
-        files: ['<%= config.src %>/{content,data,templates}/{layouts,pages,partials}/{,*/}*.{md,hbs,yml}'],
-        tasks: ['newer:assemble']
+        files: ['<%= config.src %>/{content,data,templates}/{layouts,pages,partials}/{,*/_}*.{md,hbs,yml}'],
+        tasks: ['assemble']
+      },
+      sass: {
+        files: '<%= config.src %>/stlyes/*.sass',
+        tasks: ['sass']
+      },
+      cssmin: {
+        files: '<%= config.dist %>/assets/css/*.{css}',
+        tasks: ['cssmin']
       },
       concat: {
       files: '<%= config.src %>/js/{,*/}*.js',
-        tasks: ['concat']
+      tasks: ['concat']
       },
       livereload: {
         options: {
@@ -103,21 +121,6 @@ module.exports = function(grunt) {
       }
     },
 
-    assemble: {
-      pages: {
-        options: {
-          flatten: true,
-          assets: '<%= config.dist %>/assets',
-          layout: '<%= config.src %>/templates/layouts/default.hbs',
-          data: '<%= config.src %>/data/*.{json,yml}',
-          partials: '<%= config.src %>/templates/partials/*.hbs'
-        },
-        files: {
-          '<%= config.dist %>/': ['<%= config.src %>/templates/pages/*.hbs']
-        }
-      }
-    },
-
     // Before generating any new files,
     // remove any previously-created files.
     clean: ['<%= config.dist %>/**/*.{html,xml}']
@@ -127,22 +130,18 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('assemble');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-autoprefixer');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-newer');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-bower');
-
+  grunt.loadNpmTasks('grunt-contrib-sass');
 
   grunt.registerTask('serve', [
     'clean',
     'assemble',
+    'sass',
     'concat',
-    'copy',
-    'bower',
     'connect:livereload',
     'watch'
   ]);
@@ -150,9 +149,8 @@ module.exports = function(grunt) {
   grunt.registerTask('build', [
     'clean',
     'assemble',
+    'sass',
     'concat',
-    'copy',
-    'bower',
     'uglify',
     'cssmin'
   ]);
